6cafb76c5f58bd23d49254e16b613ca4471e8af2
emonkhan500 <khdilo47@gmail.com>
2025-06-12 22:36:58 +0600

    working

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,14 +5,8 @@ const page = () => {
     return (
         <div>
             <h1>this is contact</h1>
+<h1 className='mx text-center mt-20'>Wanna See More</h1>
 
-
-            <h1 className='mx text-center mt-20'>Wanna See More</h1>
-
-            <div>
-               <Link href='/contact/personal'> <li>Personal</li></Link>
-                <Link href='/contact/company'><li>Company</li></Link>
-            </div>
         </div>
     );
 };
