6d4e60ed450b8618eefb5c4ae5651de2ce774f22
emonkhan500 <khdilo47@gmail.com>
2025-06-15 22:07:32 +0600

    working on api

---
+++ src/app/api/users/route.js
@@ -0,0 +1,23 @@
+export async function GET(){
+    return Response.json({
+        comment
+    })
+}
+const comment=[
+    {
+        id:1,
+        name:'emon'
+    },
+    {
+        id:1,
+        name:'emon'
+    },
+    {
+        id:1,
+        name:'emon'
+    },
+    {
+        id:1,
+        name:'emon'
+    }
+]
